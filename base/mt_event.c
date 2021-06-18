8430e1c5c10e95d55bfdc745698341fb1f7a6b2c
leon.xie <372588334@qq.com>
2021-06-18 03:21:24 -0700

    event api.

--- base/mt_event.c
+++ base/mt_event.c
@@ -3,21 +3,66 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <pthread.h>
-#include "mt_event.h"
 
+#include <errno.h>
+#include <fcntl.h>
+
+#include <sys/epoll.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>		/* for select() workaround */
+#include <sys/signalfd.h>	/* struct signalfd_siginfo */
+#include <unistd.h>		/* close(), read() */
+
+
+#include "mt_event.h"
+#include "mt_log.h"
 
 
 
 struct mt_ev_ctx_s {
-	int res;
+	int running;
+	int fd;	    	/* For epoll() */
+	int maxevents;  /* For epoll() */
 };
+
+
+static int _init(mt_ev_ctx_t *ctx, int close_old)
+{
+	int fd;
+
+	fd = epoll_create1(EPOLL_CLOEXEC);
+	if (fd < 0)
+		return -1;
+
+	if (close_old)
+		close(ctx->fd);
+
+	ctx->fd = fd;
+
+	return 0;
+}
 
 
 mt_ev_ctx_t *mt_ev_create(void)
 {
-	return NULL;
+	mt_ev_ctx_t *c = NULL;
+	c = malloc(sizeof(*c));
+	RETURN_VAL_IF_FAIL(c, NULL);
+	memset(c,0,sizeof(*c));
+	return mt_ev_create2(MT_EV_MAX_EVENTS);
 }
+
+mt_ev_ctx_t *mt_ev_create2(unsigned int maxevents)
+{
+	mt_ev_ctx_t *c = NULL;
+	c = malloc(sizeof(*c));
+	RETURN_VAL_IF_FAIL(c, NULL);
+	memset(c,0,sizeof(*c));
+	c->maxevents = maxevents;
+	_init(c,0);
+	return c;
+}
+
 
 void mt_ev_destroy(mt_ev_ctx_t *c)
 {
--- include/mt_event.h
+++ include/mt_event.h
@@ -1,11 +1,15 @@
 #ifndef __MT_EVENT_H
 #define __MT_EVENT_H
 
+/* Max. number of simulateneous events */
+#define MT_EV_MAX_EVENTS  10
+
 typedef struct mt_ev_ctx_s mt_ev_ctx_t;
 
 typedef void (*mt_ev_callback_t)(mt_ev_ctx_t *c,void *arg, int events);
 
 mt_ev_ctx_t *mt_ev_create(void);
+mt_ev_ctx_t *mt_ev_create2(unsigned int maxevents);
 void mt_ev_destroy(mt_ev_ctx_t *c);
 int mt_ev_run(mt_ev_ctx_t *c);
 int mt_ev_io_add(mt_ev_ctx_t *c,mt_ev_callback_t cb,void *arg,int fd, int events);
